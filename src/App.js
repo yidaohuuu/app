ed4730eedc7f84582541be89ae607e2ccd7b5422
JianJiao <jianjiao321@gmail.com>
2019-03-25 22:20:55 -0700

    post

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function post (url, json) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(json)
+  })
+    .then(res => res.json())
+}
+
 const Test = () => {
   const [text, setText] = useState('iii')
   const onClick = () => {
@@ -9,12 +21,9 @@ const Test = () => {
     setText('changed yo' + number)
   }
   const fireIt = () => {
-    fetch('/test/save')
-      .then(res => {
-        return res.json()
-      })
+    return post('/test/post', {val: 'client'})
       .then(data => {
-        console.log('the data', data)
+        console.log('check my data out', data)
       })
   }
   return (
