c27d5f2b877f8d7d0e8a1343fa06b2b807369e27
JianJiao <jianjiao321@gmail.com>
2019-04-22 22:35:04 -0700

    Updates

---
+++ precache-manifest.4c62a41da897bf402ba18ee0c26820f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41fb1283ebc5fff3cf6e",
+    "url": "/connect/static/css/main.24c17443.chunk.css"
+  },
+  {
+    "revision": "41fb1283ebc5fff3cf6e",
+    "url": "/connect/static/js/main.b784fc0f.chunk.js"
+  },
+  {
+    "revision": "9859de8203209010f5d2",
+    "url": "/connect/static/js/runtime~main.815cc0cc.js"
+  },
+  {
+    "revision": "9186bb1fd76b4c70ca55",
+    "url": "/connect/static/js/2.e2026107.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/connect/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f4e702de802beaec20a76ca8990698da",
+    "url": "/connect/index.html"
+  }
+];
